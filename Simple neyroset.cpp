ea44fa52fe24db9f5ae0a8b5d9b47eff1a383ef6
Yaroslav <109142793+Yaroslavcoder47@users.noreply.github.com>
2022-07-12 13:26:34 +0300

    Add files via upload

---
+++ Simple neyroset.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+	double x[3] = {0.5, 1.0, 0.7};
+	double weights[3] = {0.4, 0.7, 0.5};
+	double sigmoid = (1 / (1 + pow(2.71828, -sigmoid)));
+	double y;
+	for (int i; i < 2; ++i) {
+		y = sigmoid * ((x[i] * weights[1]) + weights[2]);
+	}
+	double output = x[0] * weights[0] + x[1] * weights[1] + x[2] * weights[2] + y;
+	output = (1 / (1 + pow(2.71828, -output)));
+	if (output >= 0.8) {
+		cout << "Otvet 1" << endl;
+	}
+	else {
+		cout << "Otvet 0" << endl;
+	}
+	cout << output << " Offset: " << y << endl;
+	system("pause");
+}
